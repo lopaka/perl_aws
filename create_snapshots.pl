e9f2391c9bbeb4c91dc038a55882883752550960
Lopaka Delp <lopaka@rightscale.com>
2014-07-05 13:23:23 -0700

    Read list of volumes to snapshot from settings file.

--- create_snapshots.pl
+++ create_snapshots.pl
@@ -9,6 +9,8 @@ use POSIX qw/strftime/;
 my $date = strftime "%Y%m%d", localtime;
 my $snapshot_id;
 my $config = LoadFile('.settings.yml');
+my %volumes;
+my $volume_id;
 
 my $ec2 = Net::Amazon::EC2->new(
   region => $config->{region},
@@ -16,12 +18,18 @@ my $ec2 = Net::Amazon::EC2->new(
   SecretAccessKey => $config->{SecretAccessKey}
 );
 
-#%volumes = (
-#  'linux-lvm-1of4' => 'vol-12345678',
-#  'linux-lvm-2of4' => 'vol-abcdefgh',
-#  'linux-lvm-3of4' => 'vol-1a2b3c4d',
-#  'linux-lvm-4of4' => 'vol-a1b2c3d4'
-#  );
+%volumes = %{$config->{volumes}};
 
-$snapshot = $ec2->create_snapshot( VolumeId => 'vol-12345678', Description => 'linux-lvm-1of4' );
-$ec2->create_tags( ResourceId => $snapshot->{snapshot_id}, Tags => {Name => "linux-lvm-1of4-${date}"} );
+foreach $volume_id (keys(%volumes)) {
+  $description = $volumes{$volume_id};
+  $snapshot = $ec2->create_snapshot(
+                VolumeId => $volume_id,
+                Description => $description
+              );
+  $ec2->create_tags(
+    ResourceId => $snapshot->{snapshot_id},
+    Tags => {
+      Name => "${description}-${date}"
+    }
+  );
+}
